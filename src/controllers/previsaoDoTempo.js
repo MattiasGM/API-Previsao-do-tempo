511f198752615fe248f0f6df27bf062afd4dfaab
Mattias Gaspar <93679727+MattiasGM@users.noreply.github.com>
2024-04-30 15:52:29 -0300

    API implementada

--- src/controllers/previsaoDoTempo.js
+++ src/controllers/previsaoDoTempo.js
@@ -1,9 +1,31 @@
+require('dotenv').config();
+const axios = require("axios")
 
-const temperatura = (req, res) => {
+const listarPrevisaoLocal = async (req, res) => {
+    try {
+        const { data } = await axios(`https://api.hgbrasil.com/weather?key=${process.env.KEY}&user_ip=remote`)
 
-    return res.status(200).json({ mensagem: "sucess" })
+        return res.status(200).json(data)
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send()
+    }
+}
+
+const previsaoPorCidade = async(req, res) => {
+    try {
+        const { city_name } = req.query
+
+        const { data } = await axios(`https://api.hgbrasil.com/weather?key=${process.env.KEY}&city_name=${city_name}`)
+
+        return res.status(200).json(data)
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send()
+    }
 }
 
 module.exports = {
-    temperatura
+    listarPrevisaoLocal,
+    previsaoPorCidade
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const previsaoDoTempo = require('../controllers/previsaoDoTempo')
 
 const routes = express()
 
-routes.get('/', previsaoDoTempo)
+routes.get('/', previsaoDoTempo.listarPrevisaoLocal)
+routes.get('/temperatura', previsaoDoTempo.previsaoPorCidade)
 
 module.exports = routes
